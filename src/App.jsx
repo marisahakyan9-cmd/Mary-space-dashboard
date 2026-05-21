a4a72be96e44de2b1f1a12cacea51c082eb8c83b
Student <mari.sahakyan9@tumo.org>
2026-05-21 20:06:32 +0400

    describe what you did

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+function Counter(){
+  const [count,setCount]=useState(0)
+
+  return (
+    <button onClick={()=>setCount(count + 1)}>
+      Clicked {count} times
+    </button>
+  )
+}
+function ISSTracker(){
+  const [location,setLocation] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.wheretheiss.at/v1/satellites/25544')
+  .then(r=> r.json())
+  .then(data => setLocation(data))
+  }, [])
+  return (
+    <div className='card'>
+      <h2>ISS Position</h2>
+      { location ?(
+        <p>{location.latitude.toFixed(2)}°,{location.longitude.toFixed(2)}°</p>
+      ):(
+        <p>Loading ...</p>
+      )}
+    </div>
+  )
+}
 export default function App(){
   return (
     
@@ -7,6 +36,8 @@ export default function App(){
       <p>20.05.2026</p>
       <ISSCard/>
         <MissionBadge/>
+        <Counter/>
+        <ISSTracker/>
     </div>
   )
 }
